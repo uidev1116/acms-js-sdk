1fdd47d7fe0ca5d50b9bf6c50c5b86c8634bab16
uidev1116 <uidev1116@gmail.com>
2024-01-14 03:51:17 +0900

    build: exampleで利用するbaseUrlとapiKeyを環境変数として管理できるように修正

--- examples/node/index.js
+++ examples/node/index.js
@@ -1,12 +1,12 @@
 import { createClient } from '../../src/index.ts'
 
 const acmsClient = createClient({
-  baseUrl: "https://example.com",
-  apiKey: "YOUR_API_KEY",
+  baseUrl: import.meta.env.VITE_BASE_URL,
+  apiKey: import.meta.env.VITE_API_KEY,
 });
 
 (async () => {
-  // https://example.com/blog/api/summary_index へのHTTPリクエスト結果を取得できる
+  // https://example.com/blog/api/summary_index/ へのHTTPリクエスト結果を取得できる
   const { data } = await acmsClient.get(
     { blog: 'blog', api: 'summary_index' },
   );
